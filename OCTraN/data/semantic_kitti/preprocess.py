59a10ec5d74aabb9558eefd049953caecde56d32
Aditya NG <adityang5@gmail.com>
2023-07-19 11:39:00 +0530

    feat(OCTraN/scripts/visualization/): adding visuals

--- OCTraN/data/semantic_kitti/preprocess.py
+++ OCTraN/data/semantic_kitti/preprocess.py
@@ -10,7 +10,50 @@ import hydra
 from omegaconf import DictConfig
 import OCTraN.data.semantic_kitti.io_data as SemanticKittiIO
 from hydra.utils import get_original_cwd
-from OCTraN.data.NYU.preprocess import _downsample_label
+
+def _downsample_label(label, voxel_size=(240, 144, 240), downscale=4):
+    r"""downsample the labeled data,
+    code taken from https://github.com/waterljwant/SSC/blob/master/dataloaders/dataloader.py#L262
+    Shape:
+        label, (240, 144, 240)
+        label_downscale, if downsample==4, then (60, 36, 60)
+    """
+    if downscale == 1:
+        return label
+    ds = downscale
+    small_size = (
+        voxel_size[0] // ds,
+        voxel_size[1] // ds,
+        voxel_size[2] // ds,
+    )  # small size
+    label_downscale = np.zeros(small_size, dtype=np.uint8)
+    empty_t = 0.95 * ds * ds * ds  # threshold
+    s01 = small_size[0] * small_size[1]
+    label_i = np.zeros((ds, ds, ds), dtype=np.int32)
+
+    for i in range(small_size[0] * small_size[1] * small_size[2]):
+        z = int(i / s01)
+        y = int((i - z * s01) / small_size[0])
+        x = int(i - z * s01 - y * small_size[0])
+
+        label_i[:, :, :] = label[
+            x * ds : (x + 1) * ds, y * ds : (y + 1) * ds, z * ds : (z + 1) * ds
+        ]
+        label_bin = label_i.flatten()
+
+        zero_count_0 = np.array(np.where(label_bin == 0)).size
+        zero_count_255 = np.array(np.where(label_bin == 255)).size
+
+        zero_count = zero_count_0 + zero_count_255
+        if zero_count > empty_t:
+            label_downscale[x, y, z] = 0 if zero_count_0 > zero_count_255 else 255
+        else:
+            label_i_s = label_bin[
+                np.where(np.logical_and(label_bin > 0, label_bin < 255))
+            ]
+            label_downscale[x, y, z] = np.argmax(np.bincount(label_i_s))
+    return label_downscale
+
 
 
 def majority_pooling(grid, k_size=2):
@@ -40,23 +83,28 @@ def majority_pooling(grid, k_size=2):
     return result
 
 
-@hydra.main(config_name="../../config/OCTraN.yaml")
-def main(config: DictConfig):
+# @hydra.main(config_name="../../config/OCTraN.yaml")
+# def main(config: DictConfig):
+def main():
     scene_size = (256, 256, 32)
     sequences = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
+    # current dir
     remap_lut = SemanticKittiIO.get_remap_lut(
         os.path.join(
-            get_original_cwd(),
-            "monoscene",
+            os.getcwd(),
+            "OCTraN",
             "data",
             "semantic_kitti",
             "semantic-kitti.yaml",
         )
     )
 
+    kitti_root = "/OCTraN/dataset/semantic_kitti/data_odometry_voxels"
+    kitti_preprocess_root = "/OCTraN/dataset/semantic_kitti/preprocess"
+
     for sequence in sequences:
         sequence_path = os.path.join(
-            config.kitti_root, "dataset", "sequences", sequence
+            kitti_root, "dataset", "sequences", sequence
         )
         label_paths = sorted(
             glob.glob(os.path.join(sequence_path, "voxels", "*.label"))
@@ -64,7 +112,7 @@ def main(config: DictConfig):
         invalid_paths = sorted(
             glob.glob(os.path.join(sequence_path, "voxels", "*.invalid"))
         )
-        out_dir = os.path.join(config.kitti_preprocess_root, "labels", sequence)
+        out_dir = os.path.join(kitti_preprocess_root, "labels", sequence)
         os.makedirs(out_dir, exist_ok=True)
 
         downscaling = {"1_1": 1, "1_8": 8}
@@ -95,7 +143,7 @@ def main(config: DictConfig):
                     else:
                         LABEL_ds = LABEL
                     np.save(label_filename, LABEL_ds)
-                    print("wrote to", label_filename)
+                    # print("wrote to", label_filename)
 
 
 if __name__ == "__main__":
---
+++ OCTraN/scripts/generate_output.py
@@ -0,0 +1,114 @@
+from OCTraN.model.OCTraN import OCTraN
+from OCTraN.data.semantic_kitti.kitti_dm import KittiDataModule
+
+import hydra
+from omegaconf import DictConfig
+import torch
+import numpy as np
+import os
+from hydra.utils import get_original_cwd
+from tqdm import tqdm
+import pickle
+
+
+# @hydra.main(config_name="../config/monoscene.yaml")
+# def main(config: DictConfig):
+def main(args):
+    torch.set_grad_enabled(False)
+
+    dataset = "kitti"
+    output_path = "outputs/OCTraN_Semantic"
+    batch_size = 1
+
+    # Load pretrained models
+    model_path = os.path.expanduser(args.model_path)
+
+    # Setup dataloader
+    if dataset == "kitti":
+        feature = 64
+        project_scale = 2
+        full_scene_size = (256, 256, 32)
+
+        # data_module = KittiDataModule(
+        #     root=config.kitti_root,
+        #     preprocess_root=config.kitti_preprocess_root,
+        #     frustum_size=config.frustum_size,
+        #     batch_size=int(config.batch_size / config.n_gpus),
+        #     num_workers=int(config.num_workers_per_gpu * config.n_gpus),
+        # )
+        data_module = KittiDataModule(
+            root='/OCTraN/dataset/semantic_kitti/data_odometry_voxels',
+            preprocess_root='/OCTraN/dataset/semantic_kitti/preprocess',
+            frustum_size=8,
+            project_scale=2,
+            batch_size=batch_size,
+            num_workers=0
+        )
+        data_module.setup()
+        data_loader = data_module.val_dataloader()
+        # data_loader = data_module.test_dataloader() # use this if you want to infer on test set
+    
+    else:
+        print("dataset not support")
+
+    model = OCTraN.load_from_checkpoint(
+        model_path,
+        feature=feature,
+        project_scale=project_scale,
+        # fp_loss=config.fp_loss,
+        full_scene_size=full_scene_size,
+    )
+    model.cuda()
+    model.eval()
+
+    # Save prediction and additional data 
+    # to draw the viewing frustum and remove scene outside the room for NYUv2
+    output_path = os.path.join(output_path, dataset)
+    with torch.no_grad():
+        for batch in tqdm(data_loader):
+            print(batch.keys())
+            exit()
+            batch["img"] = batch["img"].cuda()
+            pred = model(batch)
+            y_pred = torch.softmax(pred["ssc_logit"], dim=1).detach().cpu().numpy()
+            y_pred = np.argmax(y_pred, axis=1)
+            for i in range(batch_size):
+                out_dict = {"y_pred": y_pred[i].astype(np.uint16)}
+                if "target" in batch:
+                    out_dict["target"] = (
+                        batch["target"][i].detach().cpu().numpy().astype(np.uint16)
+                    )
+
+            
+                write_path = os.path.join(output_path, batch["sequence"][i])
+                filepath = os.path.join(write_path, batch["frame_id"][i] + ".pkl")
+                out_dict["fov_mask_1"] = (
+                    batch["fov_mask_1"][i].detach().cpu().numpy()
+                )
+                out_dict["cam_k"] = batch["cam_k"][i].detach().cpu().numpy()
+                out_dict["T_velo_2_cam"] = (
+                    batch["T_velo_2_cam"][i].detach().cpu().numpy()
+                )
+
+                os.makedirs(write_path, exist_ok=True)
+                with open(filepath, "wb") as handle:
+                    pickle.dump(out_dict, handle)
+                    print("wrote to", filepath)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    # model_path
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        required=True,
+        help="path to the trained model",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
